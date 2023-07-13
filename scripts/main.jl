ffb3a885ecf5bde3737862fae3c5452b46a26a70
Oriol Colomes <oriol.colomes@gmail.com>
2023-07-13 21:56:56 +0200

    More plots

--- scripts/main.jl
+++ scripts/main.jl
@@ -43,7 +43,7 @@ for ode_solver_type in ode_solver_types
   # Time convergence
   for dt in dts
     case = "$ode_solver_type"*"-$dt"
-    params = ManufacturedLinearParams(dt=dt, ode_solver_type=ode_solver_type, ne=4)
+    params = ManufacturedLinearParams(tf=0.2,dt=dt, ode_solver_type=ode_solver_type, ne=20)
     eᵤ,eₚ = @timeit to case manufactured_linear_solver(params)
     push!(eus,eᵤ)
     push!(eps,eₚ)
@@ -52,11 +52,21 @@ for ode_solver_type in ode_solver_types
     push!(memory,TimerOutputs.allocated(to[case]))
   end
 
+  # Plotting
   eu_vs_dt = plot(dts,eus,xaxis=:log10,yaxis=:log10,label="$ode_solver_type")
-  plot!(eu_vs_dt,dts,1.0e-2 .* dts .^2,label="dt²",color=:black,ls=:dash)
-  plot!(eu_vs_dt,dts,1.0e-1 .* dts .^3,label="dt³",color=:black,ls=:dashdot)
+  plot!(eu_vs_dt,dts,1.0e-3 .* dts .^2,label="dt²",color=:black,ls=:dash)
+  plot!(eu_vs_dt,dts,1.0e-2 .* dts .^3,label="dt³",color=:black,ls=:dashdot)
   eu_vs_dt_file = plotsdir("eu_vs_dt-$ode_solver_type")
+  eu_vs_time = plot(times,eus,xaxis=:log10,yaxis=:log10,label="$ode_solver_type")
+  eu_vs_time_file = plotsdir("eu_vs_time-$ode_solver_type")
+  time_vs_dt = plot(dts,times,xaxis=:log10,yaxis=:log10,label="$ode_solver_type")
+  time_vs_dt_file = plotsdir("time_vs_dt-$ode_solver_type")
+  eu_vs_mem = plot(memory,eus,xaxis=:log10,yaxis=:log10,label="$ode_solver_type")
+  eu_vs_mem_file = plotsdir("eu_vs_memory-$ode_solver_type")
   savefig(eu_vs_dt,eu_vs_dt_file)
+  savefig(eu_vs_time,eu_vs_time_file)
+  savefig(eu_vs_mem,eu_vs_mem_file)
+  savefig(time_vs_dt,time_vs_dt_file)
 end
 
 end
