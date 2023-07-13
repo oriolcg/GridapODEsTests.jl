0d06c9f5af7713f5ba3eadacf78ff13100266636
Oriol Colomes <oriol.colomes@gmail.com>
2023-07-13 19:48:55 +0200

    Weak form and solution

--- src/beltrami_flow.jl
+++ src/beltrami_flow.jl
@@ -37,7 +37,6 @@ function beltrami_flow_solver(params)
     −a * ( exp(a*z) * sin(a*x + d*y) + exp(a*y) * cos(a*z + d*x) )
   ) * exp(−d^2t)
   u(t::Real) = x -> u(x,t)
-
   p((x, y, z)) = −a^2/2 * ( exp(2a*x) + exp(2a*y) + exp(2a*z) +
     2*sin(a*x + d*y) * cos(a*z + d*x) * exp(a*(y+z)) +
     2*sin(a*y + d*z) * cos(a*x + d*y) * exp(a*(z+x)) +
@@ -58,7 +57,7 @@ function beltrami_flow_solver(params)
   refFEᵤ = ReferenceFE(lagrangian,VectorValue{3,Float64},order)
   refFEₚ = ReferenceFE(lagrangian,Float64,order-1)
   V = TestFESpace(Ω,refFEᵤ,conformity=:H1,dirichlet_tags="boundary")
-  Q = TestFESpace(Ω,refFEₚ,conformity=:L2,constraint=:zeromean)
+  Q = TestFESpace(Ω,refFEₚ,conformity=:C0,constraint=:zeromean)
   U = TransientTrialFESpace(V,u)
   P = TrialFESpace(Q)
   Y = MultiFieldFESpace([V,Q])
@@ -73,6 +72,31 @@ function beltrami_flow_solver(params)
     writevtk(Ω,filename,cellfields=["u"=>xₕ₀[1],"p"=>xₕ₀[2]])
   end
 
+  # Measures
+  dΩ = Measure(Ω,2*order)
+
+  # Weak form
+  @unpack ν = params
+  b(a,u,v) = 0.5*( (a⋅∇(u))⋅v - (a⋅∇(v))⋅u )
+  # res(t,(u,p),(v,q)) = ∫( ∂t(u)⋅v + ν*(∇(u)⊙∇(v)) - p*(∇⋅v) + q*(∇⋅u) )dΩ
+  res(t,(u,p),(v,q)) = ∫( ∂t(u)⋅v + b(u,u,v) + ν*(∇(u)⊙∇(v)) - p*(∇⋅v) + q*(∇⋅u) )dΩ
+  op = TransientFEOperator(res,X,Y)
+
+  # ODE solver
+  @unpack ode_solver_type, dt, tf = params
+  nls = NLSolver(show_trace=true,method=:newton,iterations=15)
+  if ode_solver_type == :ThetaMethod
+    ode_solver = ThetaMethod(nls,dt,0.5)
+  end
+
+  # Solution
+  xₕₜ = solve(ode_solver,op,xₕ₀,0.0,tf)
+
+  # Postprocess
+  for (xₕ,t) in xₕₜ
+    println("t = $t\n ========================")
+  end
+
   println("Finished successfully!")
   return nothing
 end
@@ -83,12 +107,15 @@ BeltramiParams
 Struct with all required parameters in `beltrami_flow_solver`
 """
 @with_kw struct BeltramiParams
-  ode_solver::Symbol = :ThetaMethod
+  ode_solver_type::Symbol = :ThetaMethod
   a::Real = π/4
   d::Real = π/2
   ne::Integer = 2
   order::Integer = 2
   vtk_output::Bool = false
+  ν::Real = 0.01
+  dt::Real = 1.0e-2
+  tf::Real = 2.0e-2
 end
 
 end
